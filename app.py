0c53512ea5978a51b9a93b6503d20fdc0a5977b9
michalkaluzny <mkaluznypraca@gmail.com>
2026-03-27 00:46:59 +0100

    refactor: delete infinite loop from app

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ st.set_page_config(
     page_icon='📈',
     layout='wide',
 )
+
+st.experimental_rerun_interval = 1000
+
 st.markdown(
     """
     <h1 style='text-align: center; margin-top: 0;'>
@@ -32,46 +35,46 @@ with right_col:
     right_box = st.empty()
 
 
-while True:
-    btc_price = get_btc_price()
-    diff = get_price_change_since_last_close()
-    price_box.markdown(
-        f"""
-        <h2 style='font-size:48px;'>
-            {f'Price: {btc_price:.0f} USD'}
-        </h2>
-        """,
-        unsafe_allow_html=True
-    )
 
-    diff_box.markdown(
-        f"""
-        <h3 style='font-size:30px;'>
-            {f'difference {diff:.0f}'}
-        </h3>
-        """,
-        unsafe_allow_html=True
-    )
+btc_price = get_btc_price()
+diff = get_price_change_since_last_close()
+price_box.markdown(
+    f"""
+    <h2 style='font-size:48px;'>
+        {f'Price: {btc_price:.0f} USD'}
+    </h2>
+    """,
+    unsafe_allow_html=True
+)
 
-    percentage_diff.markdown(
-        f"""
-        <h3 style='font-size:30px;'>
-            {f'difference in %: {get_percentage_price_change_since_last_close():.2f}%'}
-        </h3>
-        """,
-        unsafe_allow_html=True
-    )
+diff_box.markdown(
+    f"""
+    <h3 style='font-size:30px;'>
+        {f'difference {diff:.0f}'}
+    </h3>
+    """,
+    unsafe_allow_html=True
+)
 
-    news = get_btc_news(50)
+percentage_diff.markdown(
+    f"""
+    <h3 style='font-size:30px;'>
+        {f'difference in %: {get_percentage_price_change_since_last_close():.2f}%'}
+    </h3>
+    """,
+    unsafe_allow_html=True
+)
 
-    news_text = "\n\n".join([f'***{pub_date}*** - {item['title']} \n - {item['summary']}' for pub_date, item in sorted(
-        news,
-        key=lambda x: datetime.strptime(x[0], '%d.%m.%Y %H:%M:%S'),
-        reverse=True
-    )])
-    right_box.markdown(news_text)
+news = get_btc_news(50)
 
-    time.sleep(1)
+news_text = "\n\n".join([f'***{pub_date}*** - {item['title']} \n - {item['summary']}' for pub_date, item in sorted(
+    news,
+    key=lambda x: datetime.strptime(x[0], '%d.%m.%Y %H:%M:%S'),
+    reverse=True
+)])
+right_box.markdown(news_text)
+
+time.sleep(1)
 
 
 
