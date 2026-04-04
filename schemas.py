8348c647decd735602fd8efca4c62832893d9470
michalkaluzny <mkaluznypraca@gmail.com>
2026-04-04 16:10:36 +0200

    feat: add pydantic schemas for API endpoints

---
+++ schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List
+
+class Price(BaseModel):
+    price: float
+    diff : float
+    percentage_diff: float
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class PriceHistory(BaseModel):
+    timestamps: List[str]
+    prices: List[float]
+
+class News(BaseModel):
+    title: str
+    url: str
+    summary: str
+    pub_date:str
+
+class NewsResponse(BaseModel):
+    news : List[News]
+
+
+
+
