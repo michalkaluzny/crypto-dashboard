172bc94a6aaf74515b6dae4a711f75f193cb8cca
michalkaluzny <mkaluznypraca@gmail.com>
2026-04-25 17:45:11 +0200

    refactor: clean code

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -3,14 +3,18 @@ import os
 
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-def get_ai_response(text : str) -> str:
+def get_ai_response(text: str) -> str:
     """
     Gets the AI's response
 
     :param text: text to be analyzed
     :return: AI response
     """
-    system_prompt = """You are an expert in cryptocurrencies and blockchain technology. Your goal is to explain complex technical concepts in a clear, concise, and simple way. Always base your answers on facts and up-to-date information. Under no circumstances should you provide investment or financial advice. If the user asks for market forecasts or opinions, you must explicitly state that these are only estimates and do not constitute professional financial recommendations."""
+    system_prompt = """You are an expert in cryptocurrencies and blockchain technology. Your goal is to explain complex
+     technical concepts in a clear, concise, and simple way. Always base your answers on facts and up-to-date 
+     information. Under no circumstances should you provide investment or financial advice. If the user asks for 
+     market forecasts or opinions, you must explicitly state that these are only estimates and do not constitute 
+     professional financial recommendations."""
     messages = [
             {"role": "system", "content": system_prompt}
 ,
